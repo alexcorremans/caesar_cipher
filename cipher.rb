a7838a2abd938a52fcd0d0af93e7b7be8c2b7e7b
Alexandra Corremans <alexandra.corremans@gmail.com>
2017-09-04 22:13:48 +0200

    first commit

---
+++ cipher.rb
@@ -0,0 +1,8 @@
+def caesar_cipher(text, number)
+  #split string into separate characters
+  #keep the spaces and punctuation somehow: how about if an element in the list is a letter, do something to it, otherwise ignore
+  #convert letters into ascii codes using .ord
+  #keeping the same case: is there a way of checking if a letter is upcase? => using a regex would be good
+end
+
+caesar_cipher("What a string!", 5)
