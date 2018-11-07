6c91c7f47d2f35adf90b142df86389a7d05dddeb
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-11-07 12:02:17 +1100

    fixed spacing

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -1,19 +1,19 @@
 require './cipher'
 
 describe "#caesar_cipher" do
-    it "shifts each letter the required number of positions" do
-        expect(caesar_cipher("hello",3)).to eql("khoor")
-    end
+  it "shifts each letter the required number of positions" do
+    expect(caesar_cipher("hello",3)).to eql("khoor")
+  end
 
-    it "wraps from z to a" do
-        expect(caesar_cipher("zorg",3)).to eql("cruj")
-    end
+  it "wraps from z to a" do
+    expect(caesar_cipher("zorg",3)).to eql("cruj")
+  end
 
-    it "retains upper and lower case letters" do
-        expect(caesar_cipher("Hello",3)).to eql("Khoor")
-    end
+  it "retains upper and lower case letters" do
+    expect(caesar_cipher("Hello",3)).to eql("Khoor")
+  end
 
-    it "retains spaces and other symbols" do
-        expect(caesar_cipher("What a string!",5)).to eql("Bmfy f xywnsl!")
-    end
+  it "retains spaces and other symbols" do
+    expect(caesar_cipher("What a string!",5)).to eql("Bmfy f xywnsl!")
+  end
 end
